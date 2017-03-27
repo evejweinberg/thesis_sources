c9bcd65720ce3f35a05b6d22751f71d204e1cdec
evejweonberg <evejweinberg@gmail.com>
2017-03-27 14:01:53 -0400

    second

---
+++ list.js
@@ -0,0 +1,77 @@
+var allBooks = [
+{
+'title': ' CyberPsychology',
+'cover': 'https://images-na.ssl-images-amazon.com/images/I/51JpIPyAi0L._SX352_BO1,204,203,200_.jpg',
+'type': 'Textbook',
+'position': "a",
+'left': 50,
+'translate': 50
+},
+{
+'title': 'http://www.bjfogg.com/fbm_files/page4_1.pdf',
+'cover': 'img/book1.jpg',
+'type': 'White Paper',
+'position': "b",
+'left': 50,
+'translate': 50
+},
+{
+  'title': ' Interactivity in multimedia learning: An integrated model’ by Jan L. Plass',
+'cover': 'https://images-na.ssl-images-amazon.com/images/I/51-hlKNdaDL._SX258_BO1,204,203,200_.jpg',
+'type': 'White Paper',
+ 'position': "c",
+ 'left': 50,
+ 'translate': 50
+},
+{
+    'title': ' L2 Talk by Michael Norton',
+'cover':   'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4848/1753.jpg',
+'type': 'Video',
+'position': "d",
+'left': 50,
+'translate': 50
+
+},
+{
+'title': 'UX - Persuasion and Ethics',
+'cover':   'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4848/1753.jpg',
+'type': 'Lynda Course',
+'position': "e",
+'left': 50,
+'translate': 50
+},
+{
+'title': "'Motivating, Influencing, and Persuading Users' by BJ Fogg",
+'cover':   'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4848/1753.jpg',
+'type': 'Book',
+'position': "f",
+'left': 50,
+'translate': 50
+},
+{
+'title': ' UX Magazine:Designing Effective Experiences for Financial Services',
+'cover':   'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4848/1753.jpg',
+'type': 'Book',
+'link': 'https://uxmag.com/articles/designing-effective-experiences-for-financial-services',
+'position': "f",
+'left': 50,
+'translate': 50
+},
+{
+'title': ' Emotional Design in Digital Media for Learning by Jan L Plass',
+'cover':   'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4848/1753.jpg',
+'type': 'White Paper',
+'position': "f",
+'left': 50,
+'translate': 50
+},
+{
+'title': ' Brett Victor',
+'cover':   'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4848/1753.jpg',
+'type': 'Video',
+'position': "f",
+'left': 50,
+'translate': 50
+}
+
+]
